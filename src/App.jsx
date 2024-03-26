f48084386518ae2ceef6c9b00dd1c9ed6146cbd2
Agnes Iradukunda <kundaaggy@gmail.com>
2024-03-26 19:08:49 +0200

    add padding on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-    <div className='max-w-7xl mx-auto lg:px-0 px-2'>
+    <div className='max-w-7xl mx-auto lg:px-0 px-1'>
      <Navbar/>
      <HeroPage/>
      <EventPage/>
